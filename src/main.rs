df7cd289bb23c37c380cdcb9ec8b1fee72c6df01
darrkenn <darrkenn08@gmail.com>
2025-11-08 19:29:23 +0000

    Added retrieval of cythe.yml from repository, which is then parsed into a Dockerfile. Bollard then creates a container/image and runs it

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,49 @@
 mod parse_yml;
-use crate::parse_yml::Config;
+use crate::parse_yml::{create_docker_file, retrieve_yml};
 use axum::{
     Router,
-    body::{Body, to_bytes},
-    http::{HeaderMap, StatusCode},
+    body::Bytes,
+    http::{HeaderMap, StatusCode, status},
     response::IntoResponse,
     routing::post,
 };
+use bollard::{
+    Docker, body_full,
+    query_parameters::{
+        BuildImageOptionsBuilder, CreateContainerOptions, CreateContainerOptionsBuilder,
+        RemoveContainerOptions, StartContainerOptions, WaitContainerOptions,
+    },
+    secret::ContainerCreateBody,
+};
 use chrono::Local;
 use dotenv::dotenv;
+use futures_util::stream::StreamExt;
 use hex::encode;
 use hmac::{Hmac, KeyInit, Mac};
-use log::{error, info, warn};
+use log::{debug, error, info, warn};
 use serde::Deserialize;
 use sha2::Sha256;
-use std::env;
+use std::{env, io::Cursor};
 use subtle::ConstantTimeEq;
+use tar::Builder;
+use uuid::Uuid;
 
 type HmacSha256 = Hmac<Sha256>;
 
 #[derive(Deserialize)]
 struct Payload {
+    pub r#ref: String,
     pub repository: Repository,
 }
 
 #[derive(Deserialize)]
 struct Repository {
     pub html_url: String,
+    pub full_name: String,
 }
 
-async fn webhook(headers: HeaderMap, body: Body) -> impl IntoResponse {
+async fn webhook(headers: HeaderMap, body: Bytes) -> impl IntoResponse {
     let secret = env::var("GITHUB_SECRET").unwrap_or_else(|_| "".to_string());
-
     let signature = match headers.get("X-Hub-Signature-256") {
         Some(sig) => sig.to_str().unwrap_or(""),
         None => {
@@ -40,30 +52,133 @@ async fn webhook(headers: HeaderMap, body: Body) -> impl IntoResponse {
         }
     };
 
-    let body_bytes = match to_bytes(body, usize::MAX).await {
-        Ok(bb) => bb,
-        Err(e) => {
-            error!("Couldnt convert body to bytes: {e}");
-            return (StatusCode::INTERNAL_SERVER_ERROR, "").into_response();
-        }
-    };
-
-    if !verifiy_signature(&secret, signature, &body_bytes[..]) {
+    if !verifiy_signature(&secret, signature, &body[..]) {
         warn!("Received a request with an invalid signature");
         return (StatusCode::UNAUTHORIZED, "").into_response();
     }
 
-    let payload: Payload = match serde_json::from_slice::<Payload>(&body_bytes) {
+    let payload: Payload = match serde_json::from_slice::<Payload>(&body) {
         Ok(p) => p,
-        Err(_) => {
-            warn!("Received a request with invalid json");
+        Err(e) => {
+            warn!("Received a request with invalid json: {e}");
             return (StatusCode::BAD_REQUEST, "").into_response();
         }
     };
 
-    println!("REPOSITORY URL: {}", payload.repository.html_url);
+    tokio::task::spawn(async move {
+        println!(
+            "REF: {}, URL: {}, FULL_NAME: {}",
+            payload.r#ref, payload.repository.html_url, payload.repository.full_name
+        );
 
-    (StatusCode::OK, "PUSH EVENT RECEIVED").into_response()
+        let tracked_branch = match payload.r#ref.strip_prefix("refs/heads/") {
+            Some(tb) => tb,
+            None => {
+                error!("Malformed JSON or branch isn't included");
+                return;
+            }
+        };
+
+        let cythe_yml =
+            match retrieve_yml(payload.repository.full_name, tracked_branch.to_string()).await {
+                Ok(cy) => cy,
+                Err(e) => {
+                    error!("Error when retrieving cythe.yml: {e}");
+                    return;
+                }
+            };
+        if cythe_yml.track == tracked_branch {
+            let docker_file = match create_docker_file(
+                payload.repository.html_url,
+                tracked_branch.to_string(),
+                cythe_yml,
+            ) {
+                Ok(df) => df,
+                Err(e) => {
+                    error!("{e}");
+                    return;
+                }
+            };
+            println!("{docker_file}");
+
+            runner(docker_file).await;
+        }
+    });
+
+    (StatusCode::OK, "").into_response()
+}
+
+async fn runner(docker_file: String) -> Result<(), Box<dyn std::error::Error>> {
+    let docker = Docker::connect_with_local_defaults()?;
+    let name = format!("cythe-{}", Uuid::new_v4());
+    let image_name = format!("{}:latest", name);
+
+    let mut a = Builder::new(Vec::new());
+
+    let mut header = tar::Header::new_gnu();
+    header.set_size(docker_file.len() as u64);
+    header.set_cksum();
+    a.append_data(&mut header, "Dockerfile", Cursor::new(docker_file.clone()))?;
+    let tar_data = a.into_inner()?;
+
+    let build_options = BuildImageOptionsBuilder::new()
+        .dockerfile("Dockerfile")
+        .t(&image_name)
+        .nocache(true)
+        .build();
+
+    let mut build_stream =
+        docker.build_image(build_options, None, Some(body_full(tar_data.into())));
+
+    info!("Building image");
+    while let Some(result) = build_stream.next().await {
+        match result {
+            Ok(bi) => debug!("{:?}", bi),
+
+            Err(e) => {
+                error!("Error during docker image building: {e}");
+                return Err(Box::new(e));
+            }
+        }
+    }
+    info!("Successfully built image");
+
+    let config = ContainerCreateBody {
+        hostname: Some(name.clone()),
+        image: Some(image_name),
+        user: Some("root".to_string()),
+        ..Default::default()
+    };
+
+    let options = CreateContainerOptionsBuilder::new().name(&name).build();
+    let container = docker.create_container(Some(options), config).await?;
+    info!("Created container: {}", container.id);
+
+    docker
+        .start_container(&name, None::<StartContainerOptions>)
+        .await?;
+    info!("Started container: {}", container.id);
+
+    let mut wait_stream = docker.wait_container(&name, None::<WaitContainerOptions>);
+
+    while let Some(result) = wait_stream.next().await {
+        match result {
+            Ok(status) => {
+                info!("Container exited with status: {:?}", status);
+            }
+            Err(e) => {
+                error!("{e}");
+                return Err(Box::new(e));
+            }
+        }
+    }
+
+    docker
+        .remove_container(&name, None::<RemoveContainerOptions>)
+        .await?;
+    info!("Removed container: {}", container.id);
+
+    Ok(())
 }
 
 fn verifiy_signature(secret: &str, signature: &str, body_bytes: &[u8]) -> bool {
--- src/parse_yml.rs
+++ src/parse_yml.rs
@@ -1,26 +1,39 @@
+use log::error;
+use reqwest::{Client, get};
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 pub struct Step {
     pub run: Option<String>,
     pub r#use: Option<String>,
+    pub entrypoint: Option<String>,
+    pub cmd: Option<String>,
 }
 
 #[derive(Debug, Deserialize)]
-pub struct Config {
+pub struct CytheYML {
     pub base: String,
+    pub track: String,
     pub steps: Vec<Step>,
 }
 
 #[derive(Debug)]
 pub enum YamlError {
     NotAUseCommand(String),
+    NotFound,
+    CantParse(String),
+    NotReadable(String),
 }
 
 impl std::fmt::Display for YamlError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            YamlError::NotAUseCommand(s) => write!(f, "{}", s),
+            YamlError::NotAUseCommand(s) => {
+                write!(f, "The following is not an internal command: {}", s)
+            }
+            YamlError::NotFound => write!(f, "cythe.yml not found"),
+            YamlError::CantParse(s) => write!(f, "{}", s),
+            YamlError::NotReadable(s) => write!(f, "{}", s),
         }
     }
 }
@@ -29,30 +42,82 @@ impl std::error::Error for YamlError {
     fn description(&self) -> &str {
         match self {
             YamlError::NotAUseCommand(s) => s,
+            YamlError::NotFound => "",
+            YamlError::CantParse(s) => s,
+            YamlError::NotReadable(s) => s,
         }
     }
 }
 
-pub fn create_docker_config(
+pub fn create_docker_file(
     git_url: String,
-    github_user: String,
-    config: Config,
+    tracked_branch: String,
+    config: CytheYML,
 ) -> Result<String, YamlError> {
     let mut docker_file = format!("FROM {}\nWORKDIR /app\n", config.base);
 
     for step in config.steps {
         if let Some(internal_command) = step.r#use {
             match internal_command.as_str() {
-                "ciythe-checkout" => {
+                "cythe-checkout" => {
                     docker_file.push_str(&format!("RUN git clone --depth=1 {} .\n", git_url));
                 }
                 s => return Err(YamlError::NotAUseCommand(s.to_string())),
             }
         }
-        if let Some(cmd) = step.run {
-            docker_file.push_str(&format!("RUN {}\n", cmd));
+        if let Some(run) = step.run {
+            docker_file.push_str(&format!("RUN {}\n", run));
+        }
+        if let Some(entrypoint) = step.entrypoint {
+            docker_file.push_str(&format!(r#"ENTRYPOINT ["sh", "-c", "{}"]"#, entrypoint))
+        }
+        if let Some(cmd) = step.cmd {
+            docker_file.push_str(&format!("CMD ['{}']", cmd))
         }
     }
 
     Ok(docker_file)
+}
+
+pub async fn retrieve_yml(
+    repo_full_name: String,
+    tracked_branch: String,
+) -> Result<CytheYML, YamlError> {
+    let raw_url = format!(
+        "https://raw.githubusercontent.com/{}/{}/cythe.yml",
+        repo_full_name, tracked_branch
+    );
+    let response = match Client::new()
+        .get(&raw_url)
+        .header("Cache-Control", "no-cache")
+        .send()
+        .await
+    {
+        Ok(r) => r,
+        Err(e) => {
+            error!("Network error while retrieving cythe.yml: {e}");
+            return Err(YamlError::NotFound);
+        }
+    };
+    if response.status() != 200 {
+        error!(
+            "Github responded with a non 200 status: {}",
+            response.status()
+        );
+        return Err(YamlError::NotFound);
+    };
+
+    match response.text().await {
+        Ok(t) => match serde_yaml::from_str::<CytheYML>(&t) {
+            Ok(c) => Ok(c),
+            Err(e) => {
+                error!("Cant parse cythe.yml: {e}");
+                Err(YamlError::CantParse(e.to_string()))
+            }
+        },
+        Err(e) => {
+            error!("Error reading cythe.yml: {e}");
+            Err(YamlError::NotReadable(e.to_string()))
+        }
+    }
 }
